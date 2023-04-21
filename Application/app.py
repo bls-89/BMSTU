d751179b21fd7719bf40798ac4f1273a2ce0ba19
B_S <bls89@bk.ru>
2023-04-22 01:19:28 +0500

    MPV

--- Application/app.py
+++ Application/app.py
@@ -1,33 +1,50 @@
+from flask import Flask, render_template, request, jsonify
+import pandas as pd
 from tensorflow import keras
-from flask import Flask, request, render_template
+from sklearn.preprocessing import MinMaxScaler
 import numpy as np
-
-
 app = Flask(__name__)
+
+# Загрузка модели Keras
 model_path = 'app'
-# загрузка сохраненной модели Keras
 model = keras.models.load_model(model_path)
 
-# маршрут для отображения HTML страницы
+# Создание объекта MinMaxScaler
+scaler = MinMaxScaler()
+scaler1 = MinMaxScaler()
+df = pd.read_csv('df_apriori.csv')
+y = df['Соотношение матрица-наполнитель']
+X = df.drop(['Unnamed: 0','Соотношение матрица-наполнитель'], axis = 1)
+
+# Определение маршрута для отображения HTML-страницы
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# маршрут для обработки POST запроса с введенными данными
+# Определение маршрута для обработки данных из формы HTML
 @app.route('/predict', methods=['POST'])
 def predict():
-    # получение данных из формы
+    # Получение данных из формы HTML
     input_data = request.form.to_dict()
-    # преобразование данных в массив NumPy
-    input_array = np.array(list(input_data.values())).astype(float)
-    # изменение формы массива в форму, соответствующую форме входных данных модели
-    input_array = input_array.reshape((1, 12))
-    # прогнозирование значения переменной на основе введенных данных
-    prediction = model.predict(input_array)[0][0]
-    # форматирование вывода прогноза
-    prediction_formatted = '{:.2f}'.format(prediction)
-    # возврат прогноза в виде строки
-    return prediction_formatted
+
+    # Преобразование данных в массив numpy
+    input_array = [[float(input_data['input{}'.format(i+1)]) for i in range(12)]]
+
+    # Нормализация данных
+    df_nr = scaler.fit(X)
+    input_array_normalized = scaler.transform(input_array)
+    y_nr = scaler.fit(y[:,np.newaxis])
+    # Предсказание значения целевой переменной
+    prediction = model.predict(input_array_normalized)
+
+    # Обратное преобразование данных
+    prediction_unnormalized = scaler.inverse_transform(prediction)
+
+    # Возврат результата в формате JSON
+    return jsonify({'prediction': float(prediction_unnormalized[0][0])})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
+
+#%%
