11edc9859ce783456868271255e48bc0cb03f6dc
B_S <bls89@bk.ru>
2023-04-23 16:01:38 +0500

    PLEASEEEE WOOOOOORK!!!!!

--- Application/app.py
+++ Application/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
 from tensorflow import keras
-import scikit-learn as sklearn
+import sklearn
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 app = Flask(__name__)
